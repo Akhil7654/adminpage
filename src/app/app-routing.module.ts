25eedaf11ace19245ec4375ab26c61d862f438d0
Akhil7654 <msdcr88@gmail.com>
2022-08-26 21:24:48 +0530

    routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedetailComponent } from './feedetail/feedetail.component';
+import { HomeComponent } from './home/home.component';
 import { RoutedetailComponent } from './routedetail/routedetail.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
-  {path:'',component:SidebarComponent},
+  {path:'',component:HomeComponent},
   {path:'route',component:RoutedetailComponent},
   {path:'fee',component:FeedetailComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { RoutedetailComponent } from './routedetail/routedetail.component';
 import { FeedetailComponent } from './feedetail/feedetail.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     RoutedetailComponent,
-    FeedetailComponent
+    FeedetailComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
